8eaf99d7cbef98399ae1f6500a49fcc0353e2230
Katie Cartagena <katiecartagena@hotmail.com>
2024-12-27 19:43:10 -0600

    add icons + search bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,11 +6,66 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pet Store'),
-        centerTitle: true,
-        // backgroundColor: Colors.deepPurpleAccent,
+      appBar: appBar(),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(22, 107, 104, 104),
+                  blurRadius: 40,
+                  spreadRadius: 0,
+                ),
+              ],
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white54,
+                contentPadding: EdgeInsets.all(15),
+                prefixIcon: Icon(Icons.search_rounded),
+                suffixIcon: Icon(Icons.filter_alt_outlined),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text('Pet Store'),
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      elevation: 0.5,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      actions: [
+        Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Icon(Icons.more_horiz),
+        ),
+      ],
     );
   }
 }
